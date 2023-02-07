e6bbff5b7229bc706d2885489326de88525aad62
N00BIER <roman.nmv@gmail.com>
2023-02-07 09:52:44 +0300

    try/catch update

--- SensitiveGroup.ps1
+++ SensitiveGroup.ps1
@@ -1,7 +1,9 @@
 $secgroups=@("Enterprise admins","Schema admins","Domain admins","Account operators","Server operators","Print operators","DHCP Administrators","DNSAdmins")
 
-foreach ($group in $secgroups){
-
-Get-AdGroupMember -Identity $group | where {$_.objectclass -eq 'user'} | get-ADUser -Properties LastLogonDate | select name,lastlogondate
-
+try { 
+  foreach ($group in $secgroups){
+    Get-AdGroupMember -Identity $group | where {$_.objectclass -eq 'user'} | get-ADUser -Properties LastLogonDate | select name,lastlogondate
+  }
+} catch {
+  $_.Exception.Message
 }
